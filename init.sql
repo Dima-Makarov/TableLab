32cdec67e8cea7d65a2550977d7bd1c7ccf93041
Dima-Makarov <79033065+Dima-Makarov@users.noreply.github.com>
2023-11-11 16:35:27 +0300

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,29 @@
+CREATE TABLE SoCs (
+    id INT PRIMARY KEY,
+    Model VARCHAR(255),
+    Date_out DATE,
+    Tdp FLOAT,
+    Core_count INT
+);
+
+INSERT INTO SoC (id, Model, Date_out, TDP, Core_count)
+VALUES
+    (1, 'Snapdragon 730G', '2020-01-01', 50.5, 8),
+    (2, 'Snapdragon 730', '2019-05-15', 45.2, 8),
+    (3, 'Apple A17 Pro', '2023-09-22', 60.0, 12);
+
+CREATE TABLE Phones (
+    id INT PRIMARY KEY,
+    Model VARCHAR(255),
+    Date_out DATE,
+    Used_SoC INT,
+    FOREIGN KEY (Used_SoC) REFERENCES SoC(id),
+    Camera_count INT,
+    Mass FLOAT
+);
+
+INSERT INTO Phones (id, Model, Date_out, Used_SoC, Camera_count, Mass)
+VALUES
+    (1, 'Xiaomi mi note 10', '2020-03-15', 1, 4, 150.5),
+    (2, 'Apple Iphone 15', '2021-11-20', 2, 3, 130.2),
+    (3, 'Xiaomi mi9T', '2023-05-10', 3, 3, 160.0);
